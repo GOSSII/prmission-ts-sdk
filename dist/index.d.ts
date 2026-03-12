3b64bea1d3597092f0d7d1084aafa6b3b0f343db
Marc H Benaim <marcosbenaim@Marcs-MacBook-Air.local>
2026-03-12 14:58:03 -0400

    chore: add .gitignore, remove node_modules and dist from tracking

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { PrmissionClient, formatUsdc, parseUsdc } from "./client.js";
-export { PRMISSION_ABI, ERC20_ABI } from "./abi/index.js";
-export { PermissionStatus, EscrowStatus, type Permission, type Escrow, type PermissionDetails, type EscrowDetails, type SettlementPreview, type AgentTrustProfile, type AccessCheck, type PrmissionConfig, type DataCategory, DATA_CATEGORIES, PRICE_RANGES, PROTOCOL_FEE_BPS, BPS_DENOMINATOR, DISPUTE_WINDOW_SECONDS, REVOCATION_GRACE_SECONDS, USDC_DECIMALS, ADDRESSES, } from "./types.js";
-//# sourceMappingURL=index.d.ts.map
